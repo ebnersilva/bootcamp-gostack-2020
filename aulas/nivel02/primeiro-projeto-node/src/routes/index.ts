b19b3c21d653d1e0cf0f5a5bb9498811af391905
Ebner Silva <ebner.suporte@hotmail.com>
2020-04-15 09:16:34 -0300

    feat: Classes of first application with Node.js

--- aulas/nivel02/primeiro-projeto-node/src/routes/index.ts
+++ aulas/nivel02/primeiro-projeto-node/src/routes/index.ts
@@ -1,16 +1,9 @@
 import { Router } from 'express';
 
+import appointmentsRouter from './appointments.routes';
+
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json({ message: 'Hello Go Stack' });
-});
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
