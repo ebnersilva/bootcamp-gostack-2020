40f9d19538bb07352873df17bb94751ba786bfbe
Ebner Silva <ebner.suporte@hotmail.com>
2020-04-21 14:34:39 -0300

    feat: Add class of handle with global errors

--- aulas/gobarber/iniciando-back-end/src/services/UpdateUserAvatarService.ts
+++ aulas/gobarber/iniciando-back-end/src/services/UpdateUserAvatarService.ts
@@ -1,9 +1,10 @@
 import path from 'path';
 import fs from 'fs';
 import { getRepository } from 'typeorm';
-import User from '../models/User';
 
+import User from '../models/User';
 import uploadConfig from '../config/upload';
+import AppError from '../errors/AppError';
 
 interface Request {
   user_id: string;
@@ -17,7 +18,7 @@ class UpdateUserAvatarService {
     const user = await usersRepository.findOne(user_id);
 
     if (!user) {
-      throw new Error('Only authenticate users can change avatar.');
+      throw new AppError('Only authenticate users can change avatar.', 401);
     }
 
     if (user.avatar) {
