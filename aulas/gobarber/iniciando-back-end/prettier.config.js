8be2d2775ab20676a46f1ab5d2197c54ad0de760
Ebner Silva <ebner.suporte@hotmail.com>
2020-04-21 14:14:25 -0300

    feat: Add classes of gobarber initial backend

---
+++ aulas/gobarber/iniciando-back-end/prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true, // Aspas simples
+  trailingComma: 'all', // No final dos objetos coloque vírgula
+  arrowParens: 'avoid', // Quando for arrow function com 1 parametro só, remove os parenteses
+};
---
+++ aulas/gobarber/iniciando-back-end/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '62cce21746f84f9cf3b3428be0f86846',
+    expiresIn: '1d',
+  },
+};
