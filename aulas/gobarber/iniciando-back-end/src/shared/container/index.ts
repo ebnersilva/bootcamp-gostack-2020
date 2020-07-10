6b1aaed1e45434924c47c3a59060f68e194f1038
Ebner Silva <ebner.suporte@hotmail.com>
2020-07-09 22:55:49 -0300

    feat: Save Tokens in BD - BackEndGoBarber

--- aulas/gobarber/iniciando-back-end/src/shared/container/index.ts
+++ aulas/gobarber/iniciando-back-end/src/shared/container/index.ts
@@ -6,7 +6,9 @@ import '@shared/container/providers';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
@@ -16,4 +18,9 @@ container.registerSingleton<IAppointmentsRepository>(
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
 );
